a82fe5e90a96df36809372aba1a3f1738ad4c973
aatug <aatug@overstock.com>
2022-07-29 21:02:48 -0400

    second commit

---
+++ src/test/java/Kadir/pageObjects/AdminPage.java
@@ -0,0 +1,42 @@
+package Kadir.pageObjects;
+
+import Kadir.utils.BrowserUtils;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class AdminPage extends BrowserUtils {
+
+
+
+    public AdminPage(){
+        PageFactory.initElements(driver,this);
+    }
+
+    @FindBy(xpath = "(//div[contains(text(),'Admin')])[1]")
+    private WebElement admin;
+    @FindBy(xpath = "(//div[contains(text(),'Clinic')])[1]")
+    private WebElement clinic;
+    @FindBy(xpath = "(//div[contains(text(),'Facilities')])[1]")
+    private WebElement facilities;
+    @FindBy(xpath = "//h2[@class='clearfix']")
+    private WebElement facilitiesText;
+    @FindBy(name = "adm")
+    private WebElement iframe;
+
+
+
+    public void setFacilities(){
+        Actions actions=new Actions(driver);
+        actions.click(admin).click(clinic).click(facilities).build().perform();
+    }
+    public String setFacilitiesText(){
+        driver.switchTo().frame(iframe);
+        return facilitiesText.getText();
+    }
+
+
+
+
+}
